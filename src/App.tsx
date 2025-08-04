c889e321b480fe6f3efd99e29dda266842d4bc11
MacBookUz <amirtursunov2@gmail.com>
2025-08-04 12:30:38 +0500

    Login Page qo'shildi

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import Mainpage from "./Pages/Mainpage";
+import LoginPage from "./Pages/LoginPage";
+import { useEffect } from "react";
 
 function App() {
+  const token = localStorage.getItem("TOKEN");
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (token) {
+      navigate("/");
+    } else {
+      navigate("/login");
+    }
+  }, []);
+  if (!token) {
+    return <LoginPage />;
+  }
   return (
     <Routes>
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/" element={<Mainpage />} />
     </Routes>
   );
